5fdc0e8297453b07d56b791009912e8db862b4c4
Wéllyson de Lourenço <loiz.wellyson@gmail.com>
2019-02-28 09:01:40 -0400

    Commit 1

---
+++ JavaScript/modulo 1/Calculadora/js.js
@@ -0,0 +1,43 @@
+function somar(){
+    //parseInt >> mudar a String "value" para numero inteiro Value
+    var n1 = parseInt(document.getElementById("input1").value)
+    var n2 = parseInt(document.getElementById("input2").value)
+    
+    var  resultado = n1+n2
+
+    alert(resultado)
+
+}
+
+function Subtrair(){
+    //parseInt >> mudar a String "value" para numero inteiro Value
+    var n1 = parseInt(document.getElementById("input1").value)
+    var n2 = parseInt(document.getElementById("input2").value)
+    
+    var  resultado = n1 - n2
+
+    alert(resultado)
+
+}
+
+function Multiplicar(){
+    //parseInt >> mudar a String "value" para numero inteiro Value
+    var n1 = parseInt(document.getElementById("input1").value)
+    var n2 = parseInt(document.getElementById("input2").value)
+    
+    var  resultado = n1 * n2
+
+    alert(resultado)
+
+}
+
+function Dividir(){
+    //parseInt >> mudar a String "value" para numero inteiro Value
+    var n1 = parseInt(document.getElementById("input1").value)
+    var n2 = parseInt(document.getElementById("input2").value)
+    
+    var  resultado = n1 / n2
+
+    alert(resultado)
+
+}
