b62d166860a1632044968166ebdac52424df9f60
Wéllyson de Lourenço <loiz.wellyson@gmail.com>
2019-03-08 14:17:44 -0400

    Cadastro de Clientes
    corrigimos o CSS e iniciamos as funções no menu (nav) com add.csslist JavaScript

---
+++ JavaScript/modulo 1/Cadastro Cliente/js/cssChanger.js
@@ -0,0 +1,14 @@
+function abrirMenu(){
+
+    document.getElementById("menu").classList.add("menu-aberto")
+    document.getElementById("menu").classList.remove("menu-fechado")
+
+    document.getElementById("main").classList.add("main-menu-aberto")
+    document.getElementById("main").classList.add("main-menu-fechado")
+
+    document.getElementById(".btn-menu-abrir").add("exibirBotao")
+}
+
+function fecharMenu(){
+    
+}
