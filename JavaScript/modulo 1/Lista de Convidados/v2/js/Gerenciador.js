55b2d3b88fcab2ed43646566586682a2aee84495
Wéllyson de Lourenço <loiz.wellyson@gmail.com>
2019-04-02 16:43:36 -0400

    Atualização v2 - Lista de Convidados
    CSS - Atualizado
    js - Atualizado

--- JavaScript/modulo 1/Lista de Convidados/v2/js/Gerenciador.js
+++ JavaScript/modulo 1/Lista de Convidados/v2/js/Gerenciador.js
@@ -1,14 +1,37 @@
 class Gerenciador {
     adicionar() {
+        
+        let Ehvalido = this.validar()
+
+        if (Ehvalido){
+
         let nomeConvidado = document.getElementById('inputConvidado').value
-        let div = nomeConvidado
-        this.validar()
+        let div = document.createElement("div")
+        div.innerText = nomeConvidado
+        
+        let img = document.createElement("img")
+        img.setAttribute('onclick','gerenciador.remover()')
+        img.setAttribute('src',"img/lixeira.svg")
+        img.classList.add("imagem")
+
+        document.getElementById("lista").appendChild(div)
+            div.appendChild(img)
+        this.limpar()
+        }
+
+        
     }
+    remover(imgremover){
+        document.getElementById(imgremover).remove()
+            
+    }        
     validar() {
         let nomeConvidado = document.getElementById('inputConvidado').value
         if (nomeConvidado == "") {
             alert("Digite o nome do convidado!")
-        }
+            return false
+        } 
+        return true
     }
     limpar() {
         document.getElementById('inputConvidado').value = ''
