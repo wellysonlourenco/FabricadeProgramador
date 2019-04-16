d279f83287727961e9da757cb8655720b329382e
Wéllyson de Lourenço <loiz.wellyson@gmail.com>
2019-04-16 16:06:07 -0400

    IMC_Calc

--- JavaScript/modulo 1/Calculadora IMC/js/calcular.js
+++ JavaScript/modulo 1/Calculadora IMC/js/calcular.js
@@ -1,6 +1,4 @@
-class IMC {
-
-    Calcular(){
+    function calcular(){
     var n1 = parseFloat(document.getElementById("input1").value)
     var n2 = parseFloat(document.getElementById("input2").value)
 
@@ -22,44 +20,4 @@ class IMC {
         }
         
     }
-
-    classificacao () {
-        
-        var resultado = this.Calcular()
-        var resultado = parseFloat(document.getElementById('resultado'))
     
-        if (resulado <= 18 ){
-            mensagem += "Abaixo do Peso! (IMC menor que 18) - Vermelho \n"
-        }
-        if (resultado > 18 && resultado  <=25 ){
-            mensagem += "Ideal! (IMC maior que 18 e menor que 25) - Azul \n"
-        }
-        if (resultado >25 && resultado <= 30){
-            mensagem += "Sobre o peso! (IMC maior que 25 e menor que 30) - Amarelo \n "
-        }
-        if (resultado > 30 && resultado <= 35) {
-            mensagem += "Obesidade 1! (IMC maior que 30 e menor que 35) - Laranja \n"
-        }
-        if (resultado > 35 && resultado <= 40){
-            mensagem += "Obesidade 2! (IMC maior que 35 e menor que 40) - Roxo \n"
-        }
-
-        else{
-            mensagem += "Obesidade 3! (IMC maior que 40) - Preto \n"
-        }
-    
-    
-    }
-    
-
-    limpar() {
-        document.getElementById('input1').value = ''
-        document.getElementById('input2').value = ''
-
-    }
-
-    validar (){
-
-    }
-}
-var calcular = new IMC()
--- JavaScript/modulo 1/Calculadora IMC/js/classificacao.js
+++ JavaScript/modulo 1/Calculadora IMC/js/classificacao.js
@@ -1,18 +1,28 @@
-function Classificacao () {
-    var n1 = parseFloat(document.getElementById("input1").value)
-    var n2 = parseFloat(document.getElementById("input2").value)
 
-    var resultado = n1/(n2*n2)
+    function classificacao() {
+        var n1 = parseFloat(document.getElementById("input1").value)
+        var n2 = parseFloat(document.getElementById("input2").value)
 
-    if (resulado <= 18 ){
-        mensagem += "Abaixo do Peso! (IMC menor que 18) - Vermelho \n"
+        var resultado = n1/(n2*n2)
+    
+        if (resultado <= 18 ){
+            alert("Abaixo do Peso! (IMC menor que 18) - Vermelho :" +resultado);
+        }
+        if (resultado > 18 && resultado  <=25 ){
+            alert("Ideal! (IMC maior que 18 e menor que 25) - Azul :" + resultado);
+        }
+        if (resultado >25 && resultado <= 30){
+            alert("Sobre o peso! (IMC maior que 25 e menor que 30) - Amarelo : " + resultado);
+        }
+        if (resultado > 30 && resultado <= 35) {
+            alert("Obesidade 1! (IMC maior que 30 e menor que 35) - Laranja : " + resultado);
+        }
+        if (resultado > 35 && resultado <= 40){
+            alert("Obesidade 2! (IMC maior que 35 e menor que 40) - Roxo : " + resultado);
+        }
+        else{
+            alert("Obesidade 3! (IMC maior que 40) - Preto :" + resultado);
+        }
+    
+    
     }
-    if (resultado > 18 && resultado  <=25 ){
-        mensagem += "Ideal! (IMC maior que 18 e menor que 25) - Azul \n"
-    }
-    if (resultado >25 && resultado <= 30){
-        mensagem += "Sobre o peso! (IMC maior que 25 e menor que 30) - Amarelo \n "
-    }
-
-
-}
