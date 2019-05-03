1a88e786a2ed5b45cb6fd7c55772523de2dec114
Wéllyson de Lourenço <loiz.wellyson@gmail.com>
2019-05-02 20:32:33 -0400

    encontrarNomeNoArray

---
+++ JavaScript/modulo 1/1-study_RevisaoConteudo/Exercicios/LAÇOS/encontrarNomeNoArray/v2/js/encontrarNome.js
@@ -0,0 +1,51 @@
+var listaNomes = []
+
+function buscarNome(){
+    
+    let indice = 0
+    // let nomeDigitado = document.getElementById("inputNome").value
+    
+    if (nomeDigitado == ""){
+        alert("O Campo Nome deve ser preenchido!")
+        }else{
+           
+            while(listaNomes[indice] != "jão" | "maria"  && indice < listaNomes.length){
+                
+                indice++
+
+            }
+            
+            if(indice >= listaNomes.length){
+                indice = -1
+            }
+        }
+        let spanArray = document.getElementById("array")
+        spanArray.innerText = listaNomes
+
+        let spanResultado = document.getElementById("resultado")
+        spanArray.innerText = "Indice : " + indice
+
+        document.getElementById('inputNome').value = ""
+}
+
+
+function adicionarNome(){
+    let nomeDigitado = document.getElementById("inputNome").value
+
+    
+    if(nomeDigitado == ""){
+        alert("O Campo Nome deve ser preenchido!")
+    }else{
+        listaNomes.push(nomeDigitado)
+        // listaNomes[listaNomes.length] = nomeDigitado
+        alert("Nome adicionado com sucesso!")
+    }
+
+    let spanArray = document.getElementById("array")
+    spanArray.innerText = listaNomes
+
+
+
+
+
+}
