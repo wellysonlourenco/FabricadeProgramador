ed4d22344efadcdbcf1396f6933ad41353d51679
Wéllyson de Lourenço <loiz.wellyson@gmail.com>
2021-01-10 18:35:25 -0400

    jsArrayReduce

--- JavaScript/dev-SAMURAI/basicoJS/reduceArray.js
+++ JavaScript/dev-SAMURAI/basicoJS/reduceArray.js
@@ -8,3 +8,27 @@ var calc = function(total, item, indice){
 
 var somatoria = valores.reduce( calc, 0);
 console.log(somatoria);
+
+
+//dobrar numeros
+var numeros = [1,2,3,4,5,6,7,8,9,10];
+
+var calc = function(total, item){
+    return total.concat(item*2);
+}
+
+var dobrados = numeros.reduce(calc,[]);
+console.log(dobrados);
+
+//filtrar
+
+var num = [1,15,20,10,30];
+
+var selecionados = num.reduce(function(total, item){
+    if(item > 10){
+        total.push(item)
+    }
+    return total;
+},[]);
+
+console.log(selecionados);
